c862aed04bc570d5ffa0a7664003a0310701e283
ezd1000 <edrakatos@Elisabeth>
2025-04-01 08:31:04 -0400

    Updating concatanation

--- functional_test/scripts/run_rot13_L1toL2.py
+++ functional_test/scripts/run_rot13_L1toL2.py
@@ -5,8 +5,7 @@ from optparse import OptionParser
 def doProcess(infile, outfile):
     with open(outfile, 'w') as output:
         with open(infile) as infile:
-            output.write(codecs.encode(infile.read(), 'rot_13'))
-            
+            output.write(codecs.encode(infile.read(), 'rot_13'))            
 if __name__ == '__main__':
     usage = "usage: %prog infile outfile"
     parser = OptionParser(usage=usage)
@@ -19,6 +18,6 @@ if __name__ == '__main__':
     infile = args[0]
     outfile = args[-1]
 
-    print("infile + ", infile)
-    print("outfile + ", outfile)
+    print("infile ", infile)
+    print("outfile ", outfile)
     doProcess(infile, outfile)
